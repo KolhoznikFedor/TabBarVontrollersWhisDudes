9875545026f1fd64c784f5c911555db815c36c22
KolhoznikFedor <megadyper@gmail.com>
2022-04-02 00:34:55 +0300

    commit 8

--- TabBarViewControllersDudes/TabBarViewControllersDudes/Managers/DataManager.swift
+++ TabBarViewControllersDudes/TabBarViewControllersDudes/Managers/DataManager.swift
@@ -11,10 +11,10 @@ import UIKit
 class DataManager {
     static let shared = DataManager()
     private init() {
-        dudes = [Dude(name: "Eric", price: 99.0,image: #imageLiteral(resourceName: "Eric.jpg")),
-                 Dude(name: "Kyle", price: 3.0,image: #imageLiteral(resourceName: "Kyle.jpg")),
-                 Dude(name: "Stan", price: 5.0,image: #imageLiteral(resourceName: "Stan.jpg")),
-                 Dude(name: "Kenny", price: 9999.0,image: #imageLiteral(resourceName: "Kenny.jpg"))]
+        dudes = [Dude(name: "Eric", price: 99.0, image: #imageLiteral(resourceName: "Eric.jpg")),
+                 Dude(name: "Kyle", price: 3.0, image: #imageLiteral(resourceName: "Kyle.jpg")),
+                 Dude(name: "Stan", price: 5.0, image: #imageLiteral(resourceName: "Stan.jpg")),
+                 Dude(name: "Kenny", price: 9999.0, image: #imageLiteral(resourceName: "Kenny.jpg"))]
     }
     var dudes: [Dude]
 }
--- TabBarViewControllersDudes/TabBarViewControllersDudes/Model/Dude.swift
+++ TabBarViewControllersDudes/TabBarViewControllersDudes/Model/Dude.swift
@@ -12,10 +12,39 @@ struct Dude {
     var price: Double
     var feedbacks: [Feedback] = []
     var image: UIImage?
+    
+    
+    var ratingBar: String {
+        if let rating = rating {
+            return String(repeating: "🔥", count: Int(rating.rounded(.up)))
+        } else {
+            return "The are no reviews"
+        }
+    }
+    private var rating: Double? {
+        if feedbacks.isEmpty {
+            return nil
+        } else {
+            var sum: Double = 0
+            for review in feedbacks {
+                sum += review.mark
+            }
+            return sum / Double(feedbacks.count)
+        }
+    }
 }
-
 struct Feedback {
     private let date = Date()
     let text: String
     let mark: Double
+    var ratingBar: String{
+        String(repeating: "🔥", count: Int(mark.rounded(.up)))
+    }
+    var dataString: String {
+        let dateFormated = DateFormatter()
+        dateFormated.dateStyle = .medium
+        dateFormated.timeStyle = .short
+        dateFormated.locale = Locale(identifier: "ru_RU")
+        return dateFormated.string(from: date)
+    }
 }
